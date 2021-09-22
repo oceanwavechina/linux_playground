f0ee76121fac3f8ffb075417888d32c7bd074b9f
liuyanan <>
2021-09-22 10:58:57 +0800

    add tool

---
+++ tools/gcore_test.cpp
@@ -0,0 +1,19 @@
+/*
+ * gcore_test.cpp
+ *
+ *  Created on: Sep 22, 2021
+ *      Author: liuyanan
+ */
+
+#include <unistd.h>
+
+/*
+ 使用gcore 来实时生成core 文件
+
+ 	 gcore pid
+ */
+
+int main(int argc, char **argv) {
+
+	sleep(100);
+}
